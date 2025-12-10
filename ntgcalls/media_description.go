77f4cf6d2b76b49a4f5a524b0e023c092375dc44
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-12-10 10:19:23 +0530

    Update media_description.go

--- ntgcalls/media_description.go
+++ ntgcalls/media_description.go
@@ -12,61 +12,36 @@ type MediaDescription struct {
 	Screen     *VideoDescription
 }
 
-func (ctx *MediaDescription) ParseToC() (C.ntg_media_description_struct, func()) {
+func (ctx *MediaDescription) ParseToC() (C.ntg_media_description_struct) {
 	var x C.ntg_media_description_struct
-	var cleanups []func()
 	
 	if ctx.Microphone != nil {
 		cMic := C.malloc(C.size_t(unsafe.Sizeof(C.ntg_audio_description_struct{})))
-		micStruct, micCleanup := ctx.Microphone.ParseToC()
+		micStruct := ctx.Microphone.ParseToC()
 		*(*C.ntg_audio_description_struct)(cMic) = micStruct
 		x.microphone = (*C.ntg_audio_description_struct)(cMic)
-		cleanups = append(cleanups, micCleanup)
 	}
 	
 	if ctx.Speaker != nil {
 		cSpeaker := C.malloc(C.size_t(unsafe.Sizeof(C.ntg_audio_description_struct{})))
-		speakerStruct, speakerCleanup := ctx.Speaker.ParseToC()
+		speakerStruct := ctx.Speaker.ParseToC()
 		*(*C.ntg_audio_description_struct)(cSpeaker) = speakerStruct
 		x.speaker = (*C.ntg_audio_description_struct)(cSpeaker)
-		cleanups = append(cleanups, speakerCleanup)
 	}
 	
 	if ctx.Camera != nil {
 		cCamera := C.malloc(C.size_t(unsafe.Sizeof(C.ntg_video_description_struct{})))
-		cameraStruct, cameraCleanup := ctx.Camera.ParseToC()
+		cameraStruct  := ctx.Camera.ParseToC()
 		*(*C.ntg_video_description_struct)(cCamera) = cameraStruct
 		x.camera = (*C.ntg_video_description_struct)(cCamera)
-		cleanups = append(cleanups, cameraCleanup)
 	}
 	
 	if ctx.Screen != nil {
 		cScreen := C.malloc(C.size_t(unsafe.Sizeof(C.ntg_video_description_struct{})))
-		screenStruct, screenCleanup := ctx.Screen.ParseToC()
+		screenStruct := ctx.Screen.ParseToC()
 		*(*C.ntg_video_description_struct)(cScreen) = screenStruct
 		x.screen = (*C.ntg_video_description_struct)(cScreen)
-		cleanups = append(cleanups, screenCleanup)
 	}
 	
-	cleanup := func() {
-		// Free the input strings
-		for _, c := range cleanups {
-			c()
-		}
-		// Free the struct pointers
-		if x.microphone != nil {
-			C.free(unsafe.Pointer(x.microphone))
-		}
-		if x.speaker != nil {
-			C.free(unsafe.Pointer(x.speaker))
-		}
-		if x.camera != nil {
-			C.free(unsafe.Pointer(x.camera))
-		}
-		if x.screen != nil {
-			C.free(unsafe.Pointer(x.screen))
-		}
-	}
-	
-	return x, cleanup
+	return x
 }
